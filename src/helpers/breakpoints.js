e8a7116e1a09bfb33e9f396c449a294a44498774
ronnie <ronnieyeung@fontrip.com>
2022-10-12 11:04:11 +0800

    add breakppoints

---
+++ src/helpers/breakpoints.js
@@ -0,0 +1,21 @@
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  tabletH: "992px",
+  laptop: "1024px",
+  laptopL: "1200px",
+  desktop: "1440px",
+};
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  tabletH: `(min-width: ${size.tabletH})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
