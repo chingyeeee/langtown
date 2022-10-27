df1068438c6b73603ef5dd2d7562490f414cad3b
ronnie <ronnieyeung@fontrip.com>
2022-10-27 13:47:20 +0800

    add input, select component

--- src/helpers/layout.js
+++ src/helpers/layout.js
@@ -1,6 +1,8 @@
 import { device } from "./breakpoints";
 import styled from "styled-components";
 import { colors } from "./colors";
+import { fontSize } from "./fontSize";
+import IconSelect from "../images/icons/icon-select.svg";
 
 export const PageSection = styled.section`
   padding-top: 120px;
@@ -61,4 +63,68 @@ export const Image = styled.img`
   object-fit: cover;
   vertical-align: bottom;
   border-radius: 20px;
+`;
+
+export const Input = styled.input`
+  border-radius: 3px;
+  border: 1px solid ${colors.lightGray};
+  color: ${colors.gray};
+  transition: 0.3s;
+  outline: none;
+  padding: 8px 12px;
+  font-size: ${fontSize.p3};
+  box-sizing: border-box;
+  margin-bottom: 2rem;
+  width: 100%;
+  &:focus {
+    border: 1px solid ${colors.primary};
+  }
+  @media ${device.tablet} {
+    font-size: ${fontSize.p2};
+  } ;
+`;
+
+export const Label = styled.label`
+  display: block;
+  color: ${colors.black};
+  font-size: ${fontSize.p2};
+  margin-bottom: 8px;
+  font-family: "Noto Sans TC";
+  font-weight: 500;
+  line-height: 23px;
+  letter-spacing: 0.05em;
+  @media ${device.tablet} {
+    font-size: ${fontSize.p1};
+  } ;
+`;
+export const Select = styled.select`
+  background: url(${IconSelect}) no-repeat right center transparent;
+  border-radius: 3px;
+  border: 1px solid ${colors.lightGray};
+  background-color: ${colors.white};
+  color: ${colors.gray};
+  transition: 0.3s;
+  outline: none;
+  padding: 8px 12px;
+  font-size: ${fontSize.p3};
+  box-sizing: border-box;
+  margin-bottom: 2rem;
+  appearance: none;
+  width: 100%;
+  option {
+    background-color: ${colors.white};
+    color: ${colors.gray};
+    font-family: "Noto Sans TC";
+    font-weight: 500;
+    font-size: ${fontSize.p3};
+    @media ${device.tabletH} {
+      font-size: ${fontSize.p1};
+    }
+  }
+  &:focus {
+    border: 1px solid ${colors.primary};
+  }
+  @media ${device.tablet} {
+    font-size: ${fontSize.p2};
+  } ;
 `;
