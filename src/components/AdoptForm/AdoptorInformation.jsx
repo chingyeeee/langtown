ac91f8b87c3ec2663b346f7e41221e5301b043eb
ronnie <ronnieyeung@fontrip.com>
2022-10-27 17:00:30 +0800

    add AdoptorInformation Component

---
+++ src/components/AdoptForm/AdoptorInformation.jsx
@@ -0,0 +1,114 @@
+import Button from "../Button";
+import { useState } from "react";
+import { Content, Description } from "../../helpers/typography";
+import { BtnWrapper, InputGroup } from "../../pages/AdoptorInvestigation";
+import styled from "styled-components";
+import { Input, Label, Select } from "../../helpers/layout";
+import { Col, Row } from "react-bootstrap";
+
+const Introduction = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const PhraseList = styled.div`
+  flex-grow: 1;
+`;
+
+const FormIntroduction = (props) => {
+  const { phraseList, setFormStart, prevStep } = props;
+
+  return (
+    <Introduction>
+      <PhraseList>
+        {phraseList.map((item, i) => {
+          return (
+            <Content className="mb-3" key={i}>
+              {item}
+            </Content>
+          );
+        })}
+      </PhraseList>
+      <BtnWrapper className="mt-5">
+        <Button active="true" onClick={prevStep}>
+          上一步
+        </Button>
+        <Button active="true" onClick={() => setFormStart(true)}>
+          開始填寫
+        </Button>
+      </BtnWrapper>
+    </Introduction>
+  );
+};
+
+const BasicInformation = (props) => {
+  const { content, nextStep, setFormStart, register, errors } = props;
+
+  return (
+    <>
+      <Row>
+        {content.map((input, i) => {
+          const { name, type, description } = input;
+          // console.log(pattern);
+
+          return (
+            <Col xs={12} md={6} key={i}>
+              {type === "select" ? (
+                <InputGroup>
+                  <Label name={name}>{name}</Label>
+                  <Select name={name}>
+                    {input.options.map((option, i) => {
+                      return <option key={i}>{option}</option>;
+                    })}
+                  </Select>
+                </InputGroup>
+              ) : (
+                <InputGroup>
+                  <Label htmlFor={name}>{name}</Label>
+                  <Description>{description}</Description>
+                  <Input type={type} {...register(name, { required: true })} />
+                  {console.log(errors)}
+                </InputGroup>
+              )}
+            </Col>
+          );
+        })}
+      </Row>
+      <BtnWrapper>
+        <input type="submit" />
+        <Button active="true" onClick={() => setFormStart(false)}>
+          上一步
+        </Button>
+        <Button onClick={nextStep}>下一步</Button>
+      </BtnWrapper>
+    </>
+  );
+};
+
+export const AdoptorInformation = (props) => {
+  const { phrase, nextStep, prevStep, register, errors, onSubmit } = props;
+  const { content, phraseList } = phrase;
+  const [formStart, setFormStart] = useState(false);
+
+  return (
+    <>
+      {formStart ? (
+        <BasicInformation
+          content={content}
+          setFormStart={setFormStart}
+          nextStep={nextStep}
+          register={register}
+          errors={errors}
+          onSubmit={onSubmit}
+        />
+      ) : (
+        <FormIntroduction
+          phraseList={phraseList}
+          setFormStart={setFormStart}
+          prevStep={prevStep}
+        />
+      )}
+    </>
+  );
+};
