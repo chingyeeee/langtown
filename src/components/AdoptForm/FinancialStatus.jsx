19eb5bffbeb32e116b8b3615f45b41ed46903b78
ronnie <ronnieyeung@fontrip.com>
2022-10-29 09:40:38 +0800

    add FinancialStatus Component

---
+++ src/components/AdoptForm/FinancialStatus.jsx
@@ -0,0 +1,74 @@
+import { Col, Row } from "react-bootstrap";
+import { BtnWrapper } from "../../pages/AdoptorInvestigation";
+import Button from "../Button";
+import { Description, ValidationMsg } from "../../helpers/typography";
+import { InputGroup } from "../../pages/AdoptorInvestigation";
+import { Label, Input, Select } from "../../helpers/layout";
+import { ErrorMessage } from "@hookform/error-message";
+
+export const FinancialStatus = (props) => {
+  const { phrase, nextStep, register, errors, prevStep } = props;
+  const { content } = phrase;
+
+  return (
+    <>
+      <Row>
+        {content.map((input, i) => {
+          const {
+            name,
+            type,
+            description,
+            required,
+            pattern,
+            placeholder,
+            options,
+          } = input;
+          return (
+            <Col xs={12} key={i}>
+              {type === "select" ? (
+                <InputGroup>
+                  <Label htmlFor={name}>{name}</Label>
+                  <Select name={name}>
+                    {options.map((option, i) => {
+                      return <option key={i}>{option}</option>;
+                    })}
+                  </Select>
+                </InputGroup>
+              ) : (
+                <InputGroup>
+                  <Label htmlFor={name}>{name}</Label>
+                  <Description>{description}</Description>
+                  <Input
+                    type={type}
+                    placeholder={placeholder}
+                    {...register(name, {
+                      required: required,
+                      pattern: pattern,
+                    })}
+                  />
+
+                  <ErrorMessage
+                    errors={errors}
+                    name={name}
+                    render={({ message }) => (
+                      <ValidationMsg>{message}</ValidationMsg>
+                    )}
+                  />
+                </InputGroup>
+              )}
+            </Col>
+          );
+        })}
+      </Row>
+      <BtnWrapper>
+        <input type="submit" />
+        <Button active="true" onClick={prevStep}>
+          上一步
+        </Button>
+        <Button active="true" onClick={nextStep}>
+          下一步
+        </Button>
+      </BtnWrapper>
+    </>
+  );
+};
